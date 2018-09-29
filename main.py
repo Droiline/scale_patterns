9797cd73c3603f79e6cc7f46974de60e2b017a00
Droiline <annaruthgrace@hotmail.com>
2018-09-29 22:46:49 +0100

    Activator and inhibitor neighbours are no longer hardcoded. Possibly make them variable with the x values later?

--- main.py
+++ main.py
@@ -16,23 +16,28 @@ from mask import sml_mask, med_mask
 
 iters = 9
 dims = (80,50)
-# dims = (6,8)
+# dims = (15,15)
 
-pattern = pattern_gen(dims, iter_n=iters)
-# print(pattern)
+pattern = pattern_gen(dims, iter_n=iters, ar=2, ir=4)
 
 cols = m.ceil(m.sqrt(iters))
 rows = m.ceil(iters/cols)
 fig, ax = plt.subplots(rows, cols)
 fig.tight_layout()
 
-for i in range(iters):
-    scales = scalify.scalify(pattern[i], med_mask)
-    col = i//cols
-    row = i%cols
-    ax[col,row].pcolormesh(scales, cmap='YlOrBr')
-    ax[col,row].axis('off')
-    ax[col,row].axis('equal')
-    ax[col,row].set_title(i)
+if cols > 1:
+    for i in range(iters):
+        scales = scalify.scalify(pattern[i], med_mask)
+        col = i//cols
+        row = i%cols
+        ax[col,row].pcolormesh(scales, cmap='YlOrBr')
+        ax[col,row].axis('off')
+        ax[col,row].axis('equal')
+        ax[col,row].set_title(i)
+else:
+    scales = scalify.scalify(pattern[0], med_mask)
+    ax.pcolormesh(scales, cmap='YlOrBr')
+    ax.axis('off')
+    ax.axis('equal')
 
 plt.show()
--- pattern.py
+++ pattern.py
@@ -8,16 +8,38 @@ def init_even(x, shape):
 def init_wideback(x, shape):
     return 0.96*(m.cos(x*2*m.pi/(shape[1]-1))-0.19*m.cos(x*4*m.pi/(shape[1]-1))-0.105*m.cos(x*6*m.pi/(shape[1]-1))-0.045*m.cos(x*8*m.pi/(shape[1]-1)))
 
-def compound_cosine(x, shape, *harmonic):
+def compound_cosine(x, shape, amplitude, harmonics):
     compound = 0
-    for h in harmonic:
-        compound += h*m.cos(x*2*m.pi/(shape[1]-1))
+    for i, h in enumerate(harmonics):
+        compound += h*m.cos(x*2*(i+1)*m.pi/(shape[1]-1))
+
+    return amplitude*compound
+
+# Calculates the sum of the diamond neighbours from the square slice given
+def sum_neighbours(nbhood, osc):
+    r = nbhood.shape[0]//2
+    x = -r
+    sum = 0
+    for y in range(1, r+1, 2):
+        sum += np.sum(nbhood[r-y:r+y+1, r+x*osc])
+        x += 1
+    sum += np.sum(nbhood[:, r-r//2:r+r//2+1])
+    x = r
+    for y in range(0, r, 2):
+        sum += np.sum(nbhood[r-y:r+y+1, r+x*osc])
+        x -= 1
+    return sum
 
 def pattern_gen(shape, iter_n=2, stoc=0.1, ar=2, ir=4):
     # Generate list of numbers between 0 and 1 that follow a cosine curve. Use these
     # as inhibitor concentration values.
-    inhib_c = [(init_even(x, shape)+1)/5 for x in range(shape[1])]
-    #print(inhib_c)
+    harms = [1]
+    # harms = [1, -0.19, -0.105, 0.045]
+    # harms = [1, -0.315, -0.055, 0.055]
+    # inhib_c = [(compound_cosine(x, shape, 0.2, harms)+1)/5 for x in range(shape[1])]
+    # inhib values from 0.23 - 0.45 are good
+    inhib_c = [0.3]*shape[1]
+    print(inhib_c)
     # Use init_probs to generate the initial scale states
     #output = [np.array([r.choices([1,0], cum_weights=[prob,1], k=shape[0]) for prob in init_probs]).transpose())]
     # Pad out the array by the inhibitor radius on all sides
@@ -34,23 +56,17 @@ def pattern_gen(shape, iter_n=2, stoc=0.1, ar=2, ir=4):
         # Copy the horizontal boundaries into the padding, this will make the array wrap
         back[:,:ir] = back[:,-2*ir:-ir].copy()
         back[:,-ir:] = back[:,ir:2*ir].copy()
-        for y in range(ir-1, shape[0]+ir):
-            for x in range(ir-1, shape[1]+ir):
-                ad = (np.sum(back[y-1:y+2, x-2*osc]) +
-                      np.sum(back[y-2:y+3, x-1:x+2]) +
-                             back[y, x+2*osc])
-                id = (np.sum(back[y-1:y+2, x-4*osc]) +
-                      np.sum(back[y-3:y+4, x-3*osc]) +
-                      np.sum(back[y-4:y+5, x-2:x+3]) +
-                      np.sum(back[y-2:y+3, x+3*osc]) +
-                             back[y, x+4*osc])
+        for y in range(ir, shape[0]+ir):
+            for x in range(ir, shape[1]+ir):
+                ad = sum_neighbours(back[y-ar:y+ar+1, x-ar:x+ar+1], osc)
+                id = sum_neighbours(back[y-ir:y+ir+1, x-ir:x+ir+1], osc)
                 # print(ad, inhib_c[x-ir], id)
                 if ad - inhib_c[x-ir]*id > 0:
                     # print("0")
-                    front[y,x] = 0
+                    front[y,x] = 1
                 elif ad - inhib_c[x-ir]*id < 0:
                     # print("1")
-                    front[y,x] = 1
+                    front[y,x] = 0
                 else:
                     # print("stay")
                     front[y,x] = back[y,x].copy()
